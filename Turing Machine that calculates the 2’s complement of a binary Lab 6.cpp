bbccdcd6142f0f4b16fcb6e27eb0e7857efb00fa
penguininnepal <111071794+penguininnepal@users.noreply.github.com>
2024-04-11 18:24:46 +0545

    Update Turing Machine that calculates the 2’s complement6.cpp

--- Turing Machine that calculates the 2’s complement of a binary Lab 6.cpp
+++ Turing Machine that calculates the 2’s complement of a binary Lab 6.cpp
@@ -1,74 +1,87 @@
-//Lab 6: Design a Turing Machine that calculates the 2’s complement of a binary 
-//string over the input symbol = {0,1}.
-#include <iostream.h>
-#include <conio.h>
-#include <stdio.h>
-void main()
-{
+/*
+Design a Turing Machine that calculates the 2?s complement of a binary
+string over the input symbol, E = {0,1}.
+*/
+#include <iostream>
+using namespace std;
+
 char Input[10];
-clrscr();
-cout<<"Enter input binary string\n";
-gets(Input);
-int i=0;
-q0:
-if(Input[i]=='0'|| Input[i]=='1')
-{
-i++;
-goto q0;
-}
-else if(Input[i]=='\0')
-{
-i--;
-goto q1;
-}
-else
-{
-goto Invalid;
-}
-q1:
-if(Input[i]=='0')
-{
-i--;
-goto q1;
-}
-else if(Input[i]=='1')
-{
-i--;
-goto q2;
-}
-else
-{
-goto Invalid;
-}
-q2:
-if(Input[i]=='0')
-{
-Input[i]='1';
-i--;
-goto q2;
-}
-else if(Input[i]=='1')
-{
-Input[i]='0';
-i--;
-goto q2;
-}
-else if(i== -1)
-{
-goto q3;
-}
-else
-{
-goto Invalid;
-}
-q3:
-cout<<"\nOutput: Two's complement is";
-puts(Input);
-goto exit;
-Invalid:
-cout<<"\n You have entered some invalid string.";
-goto exit;
-exit:
-getch();
+
+void q0(int);
+void q1(int);
+void q2(int);
+void q3();
+
+void q0(int i){
+	if(Input[i]=='0'|| Input[i]=='1')
+	{
+		q0(i + 1);
+	}
+		else if(Input[i]=='\0')
+	{
+		q1(i - 1);
+	}
+	else
+	{
+		cout<<"Output: Invalid string.";
+		exit(0);
+	}
 }
 
+void q1(int i){
+	if(Input[i]=='0')
+	{
+		q1(i - 1);
+	}
+	else if(Input[i]=='1')
+	{
+		q2(i - 1);
+	}
+	else
+	{
+		cout<<"Output: Invalid string.";
+		exit(0);
+	}
+}
+
+void q2(int i){
+	if(Input[i]=='0')
+	{
+		Input[i]='1';
+		q2(i - 1);
+	}
+	else if(Input[i]=='1')
+	{
+		Input[i]='0';
+		q2(i - 1);
+	}
+	else if(i == -1)
+	{
+		q3();
+	}
+	else
+	{
+		cout<<"Output: Invalid string.";
+		exit(0);
+	}
+}
+
+void q3(){
+	cout<<"Output String: ";
+	puts(Input);
+	exit(0);
+}
+
+
+int main(){
+cout<<"Enter binary string: ";
+gets(Input);
+q0(0);
+return 0;
+}
+
+/*
+Output
+Enter binary string: 101101001
+Output String: 010010111
+*/
