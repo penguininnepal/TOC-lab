d44e214fc5f8d83db59eb7524e37669d0ed21b8e
penguininnepal <111071794+penguininnepal@users.noreply.github.com>
2024-04-11 18:30:05 +0545

    Update Finite automata that accepts all strings having 3 consecutive 1's Lab 7.cpp

--- Finite automata that accepts all strings Lab 7.cpp
+++ Finite automata that accepts all strings Lab 7.cpp
@@ -1,95 +1,95 @@
-Lab 7: Design a Finite automata that accepts all strings over input symbol, ?= {0, 
-1} having three consecutive 1's as a substring. 
-#include <iostream.h>
-#include <conio.h>
-#include <stdio.h>
-void main()
-{
+/*
+Design a Finite automata that accepts all strings over input symbol,
+E = {0,1} having three consecutive 1's as a substring.
+*/
+#include <iostream>
+using namespace std;
 char Input[10];
-clrscr();
-cout<<"Enter a string to validate (input string should be of 0 and 1)\n";
-gets(Input);
-int i=-1;
-A:
-i++;
-if(Input[i]=='0')
+
+void A(int);
+void B(int);
+void C(int);
+void D(int);
+
+void A(int i){
+	if(Input[i]=='0')
+	{
+		A(i + 1);
+	}
+	else if(Input[i]=='1')
+	{
+		B(i + 1);
+	}
+	else
+	{
+		cout<<"\n Output: Invalid String";
+		exit(0);
+	}
+}
+
+void B(int i){
+	if(Input[i]=='0')
+	{
+		A(i + 1);
+	}
+	else if(Input[i]=='1')
+	{
+		C(i + 1);
+	}
+	else
+	{
+		cout<<"\n Output: Invalid String";
+		exit(0);
+	}
+}
+
+void C(int i){
+	if(Input[i]=='0')
+	{
+		A(i + 1);
+	}
+	else if(Input[i]=='1')
+	{
+		D(i + 1);
+	}
+	else
+	{
+		cout<<"\n Output: Invalid String";
+		exit(0);
+	}
+}
+
+void D(int i){
+	if(Input[i]=='0')
+	{
+		D(i + 1);
+	}
+	else if(Input[i]=='1')
+	{
+		D(i + 1);
+	}
+	else if(Input[i]=='\0')
+	{
+		cout<<"\n Output: Valid String";
+		exit(0);
+	}
+	else
+	{
+		cout<<"\n Output: Invalid String";
+		exit(0);
+	}
+}
+
+int main()
 {
-goto A;
+	cout<<"Enter binary string: ";
+	gets(Input);
+	A(0);
+	return 0;
 }
-else if(Input[i]=='1')
-{
-goto B;
-}
-else if(Input[i]=='\0')
-{
-goto Invalid;
-}
-else
-{
-goto Wrong;
-}
-B:
-i++;
-if(Input[i]=='0')
-{
-goto A;
-}
-else if(Input[i]=='1')
-{
-goto C;
-}
-else if(Input[i]=='\0')
-{
-goto Invalid;
-}
-else
-{
-goto Wrong;
-}
-C:
-i++;
-if(Input[i]=='0')
-{
-goto A;
-}
-else if(Input[i]=='1')
-{
-goto D;
-}
-else if(Input[i]=='\0')
-{
-goto Invalid;
-}
-else
-{
-goto Wrong;
-}
-D:
-i++;
-if(Input[i]=='0')
-{
-goto D;
-}
-else if(Input[i]=='1')
-{
-goto D;
-}
-else if(Input[i]=='\0')
-{
-goto Valid;
-}
-else
-{
-goto Wrong;
-}
-Valid:
-cout<<"\n Output: Valid String";
-goto exit;
-Invalid:
-cout<<"\n Output: Invalid String";
-goto exit;
-Wrong:
-cout<<"\n Please enter binary string {format of 0, 1}";
-exit:
-getch();
-}
+
+/*
+output
+Enter binary string: 111011100111
+Output: Valid String
+*/
