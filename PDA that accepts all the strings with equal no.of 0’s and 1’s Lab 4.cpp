762ab1ba46069b58e0ae006f23e0c8e8a87b8668
penguininnepal <111071794+penguininnepal@users.noreply.github.com>
2024-04-11 17:57:02 +0545

    Update PDA that accepts all the strings with equal no.of 0’s and 1’s Lab 4.cpp

--- PDA that accepts all the strings with equal no.of 0’s and 1’s Lab 4.cpp
+++ PDA that accepts all the strings with equal no.of 0’s and 1’s Lab 4.cpp
@@ -1,62 +1,66 @@
-// Lab-4 Design a PDA that accepts all the strings with equal no.of 0’s and 1’s over 
+// Lab-4 Design a PDA that accepts all the strings with equal no.of 0â€™s and 1â€™s over 
 //the input symbol, ?={0,1} for a language, L=0n1n/ n>=1.
-#include <iostream.h>
+#include <iostream>
 #include <conio.h>
 #include <stdio.h>
-void main()
+
+int main()
 {
-char Input[10];
-char stack[10]; //Implementing stack through array.
-int Top = -1;
-clrscr();
-cout<<"Enter binary string to validate (input string should be of 0 and 
-1)\n";
-gets(Input);
-stack[++Top] = 'Z';//Taking 'Z'as an initial stack symbol.
-int i=-1;
+    char Input[10];
+    char stack[10]; // Implementing stack through array.
+    int Top = -1;
+
+    std::cout << "Enter a binary string to validate (input string should consist of 0s and 1s)\n";
+    gets(Input);
+
+    stack[++Top] = 'Z'; // Taking 'Z' as the initial stack symbol.
+    int i = -1;
+
 q0:
-i++;
-if(Input[i]=='0' && stack[Top]== 'Z')
-{
-stack[++Top]= 'A';
-goto q0;
-}
-else if(Input[i]=='0' && stack[Top]== 'A')
-{
-stack[++Top]= 'A';
-goto q0;
-}
-else if(Input[i]=='1' && stack[Top]== 'A')
-{
-Top--;
-goto q1;
-}
-else
-{
-goto Invalid;
-}
+    i++;
+    if (Input[i] == '0' && stack[Top] == 'Z')
+    {
+        stack[++Top] = 'A';
+        goto q0;
+    }
+    else if (Input[i] == '0' && stack[Top] == 'A')
+    {
+        stack[++Top] = 'A';
+        goto q0;
+    }
+    else if (Input[i] == '1' && stack[Top] == 'A')
+    {
+        Top--;
+        if (Input[i + 1] == '\0' && stack[Top] == 'Z')
+        {
+            std::cout << "\nOutput: Valid String";
+            return 0;
+        }
+        goto q1;
+    }
+    else
+    {
+        goto Invalid;
+    }
+
 q1:
-i++;
-if(Input[i]=='1' && stack[Top]== 'A')
-{
-Top--;
-goto q1;
-}
-else if(Input[i]=='\0' && stack[Top]== 'Z')
-{
-Top--;
-goto q2;
-}
-else
-{
-goto Invalid;
-}
-q2:
-cout<<"\n Output: Valid String";
-goto exit;
+    i++;
+    if (Input[i] == '1' && stack[Top] == 'A')
+    {
+        Top--;
+        if (Input[i + 1] == '\0' && stack[Top] == 'Z')
+        {
+            std::cout << "\nOutput: Valid String";
+            return 0;
+        }
+        goto q1;
+    }
+    else
+    {
+        goto Invalid;
+    }
+
 Invalid:
-cout<<"\n Output: Invalid String";
-goto exit;
-exit:
-getch();
+    std::cout << "\nOutput: Invalid String";
+    return 0;
 }
